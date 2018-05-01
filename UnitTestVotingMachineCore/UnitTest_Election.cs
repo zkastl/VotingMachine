65cae9549746f61e8cfae373af79d11b87589915
Zachary Kastl <zkastl8@gmail.com>
2018-04-30 21:01:33 -0500

    initial commit

---
+++ UnitTestVotingMachineCore/UnitTest_Election.cs
@@ -0,0 +1,131 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VotingMachineCore;
+
+namespace UnitTestVotingMachineCore
+{
+    [TestClass]
+    public class UnitTest_Election
+    {
+        List<int> eligibleVoters;
+        List<string> candidates;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            eligibleVoters = new List<int>()
+            {
+                12345,
+                67890,
+                13579,
+                24680,
+                10293,
+                48576
+            };
+            candidates = new List<string>()
+            {
+                "Zak Kastl",
+                "Vy Vo",
+                "Aaron Beal",
+                "Barry Hunt"
+            };
+        }
+
+        [TestMethod]
+        public void TestCreateElection()
+        {
+            Election testElection = new Election("TEST ELECTION",
+                eligibleVoters, candidates);
+            Assert.IsNotNull(testElection);
+        }
+
+        [TestMethod]
+        public void TestVote_Successful()
+        {
+            string expectedResult = "Vote Accepted!";
+            Election testElection = new Election("TEST ELECTION",
+                eligibleVoters, candidates);
+
+            Assert.AreEqual(expectedResult,
+                testElection.ProcessVote(12345, new List<string>() { "Zak Kastl" }));
+        }
+
+        [TestMethod]
+        public void TestVote_Successful_MultipleCandidates()
+        {
+            string expectedResult = "Vote Accepted!";
+            Election testElection = new Election("TEST ELECTION",
+                eligibleVoters, candidates);
+
+            Assert.AreEqual(expectedResult,
+                testElection.ProcessVote(12345, new List<string>() { "Zak Kastl", "Vy Vo" }));
+        }
+
+        [TestMethod]
+        public void TestVote_Unsuccessful_BadCandidate()
+        {
+            string expectedResult = "Invalid Candidate(s)";
+            Election testElection = new Election("TEST ELECTION",
+                eligibleVoters, candidates);
+
+            Assert.AreEqual(expectedResult, 
+                testElection.ProcessVote(12345, new List<string>() { "Bilbo Baggins" }));
+        }
+
+        [TestMethod]
+        public void TestVote_Unsuccessful_BadCandidate_Multiple()
+        {
+            string expectedResult = "Invalid Candidate(s)";
+            Election testElection = new Election("TEST ELECTION",
+                eligibleVoters, candidates);
+
+            Assert.AreEqual(expectedResult,
+                testElection.ProcessVote(12345, new List<string>() {"Zak Kastl", "Bilbo Baggins" }));
+        }
+
+        [TestMethod]
+        public void TestVote_Unsuccessful_IneligibleVoter()
+        {
+            string expectedResult = "This User is not eligible to vote.";
+            Election testElection = new Election("TEST ELECTION",
+                eligibleVoters, candidates);
+
+            Assert.AreEqual(expectedResult,
+                testElection.ProcessVote(00000, new List<string>() { "Zak Kastl" }));
+        }
+
+        [TestMethod]
+        public void TestVote_Unsuccessful_VotedTwice()
+        {
+            string expectedResult = "User cannot vote more than once per election";
+            Election testElection = new Election("TEST ELECTION",
+                eligibleVoters, candidates);
+
+            testElection.ProcessVote(12345, new List<string>() { "Zak Kastl" });
+
+            Assert.AreEqual(expectedResult,
+                testElection.ProcessVote(12345, new List<string>() { "Zak Kastl" }));
+        }
+
+        [TestMethod]
+        public void Test_AllVotersVoted_True()
+        {
+            Election testElection = new Election("TEST ELECTION",
+                eligibleVoters, candidates);
+
+            foreach (int voter in eligibleVoters)
+                testElection.ProcessVote(voter, new List<string>() { "Zak Kastl" });
+
+            Assert.IsTrue(testElection.HaveAllVotersVoted());
+        }
+
+        [TestMethod]
+        public void Test_AllVotersVoted_False()
+        {
+            Election testElection = new Election("TEST ELECTION",
+                eligibleVoters, candidates);
+
+            Assert.IsFalse(testElection.HaveAllVotersVoted());
+        }
+    }
+}
---
+++ VotingMachineCore/Election.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace VotingMachineCore
+{
+    public class Election
+    {
+        public Dictionary<int, List<string>> Vote { get; private set; }
+        public string Title { get; private set; }
+        public List<int> EligibleVoters { get; private set; }
+        public List<string> Candidates { get; private set; }
+        public int CandidateCount { get; private set; }
+
+        public Election(string title, List<int> eligibleVoters, int candidateCount = 1, List<string> candidates = null)
+        {
+            Vote = new Dictionary<int, List<string>>();
+            Title = title;
+            EligibleVoters = new List<int>(eligibleVoters);
+            Candidates = candidates ?? new List<string>();
+        }
+
+        public string ProcessVote(int voterId, List<string> candidates)
+        {
+            if (!EligibleVoters.Contains(voterId))
+                return "This User is not eligible to vote.";
+            else if (candidates.Where(x => !Candidates.Contains(x)).Count() > 0)
+                return "Invalid Candidate(s)";
+            else if (Vote.ContainsKey(voterId))
+                return "User cannot vote more than once per election";
+            else
+            {
+                Vote.Add(voterId, candidates);
+                return "Vote Accepted!";
+            }
+        }
+
+        public bool HaveAllVotersVoted() =>
+            EligibleVoters.Where(x => !Vote.ContainsKey(x)).Count() == 0;
+    }
+}
